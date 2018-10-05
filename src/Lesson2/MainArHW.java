d400a2470ba0c911e1570ff7d57534412bd0f809
Ivanov Evgenii <ivanoff_john@mail.ru>
2018-10-05 15:58:57 +0300

    Added class work

---
+++ src/Lesson2/MainArHW.java
@@ -0,0 +1,19 @@
+package Algorithm_HW.src.Lesson2;
+
+public class MainArHW {
+    public static void main(String[] args) {
+        /*int[] arr = {1,2,3,4,5};
+        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));*/
+
+        Array arr = new Array(5);
+        for (int i = 0; i < 10; i++) {
+            arr.append(i);
+        }
+
+        for (int i = 0; i < arr.length(); i++) {
+            System.out.printf("%3d", arr.get(i));
+        }
+
+    }
+}

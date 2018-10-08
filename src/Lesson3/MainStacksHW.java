1ded764b8b8da8efaa16e45bddd7eb90c89ac922
Ivanov Evgenii <ivanoff_john@mail.ru>
2018-10-08 15:17:04 +0300

    Added method reverse for reversing input string

--- src/Lesson3/MainStacksHW.java
+++ src/Lesson3/MainStacksHW.java
@@ -1,4 +1,21 @@
 package Lesson3;
 
 public class MainStacksHW {
+    public static void main(String[] args) {
+        String someStr = "abcdefg";
+
+        reverse(someStr);
+    }
+
+    public static void reverse(String str){
+        int size = str.length();
+        Stack stack = new Stack(size);
+        for (int i = 0; i < size; i++) {
+            char ch = str.charAt(i);
+            stack.insert(ch);
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print((char) stack.remove());
+        }
+    }
 }
